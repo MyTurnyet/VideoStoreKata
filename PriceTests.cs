e3c4e9fb8cbc9fff4c9376d0b2fb13af8050be2d
Paige Watson <paige.watson@outlook.com>
2022-04-21 12:12:37 -0700

    add price tests for 1,2,& 3 days

---
+++ PriceTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoStore;
+
+[TestClass]
+public class PriceTests
+{
+    [TestMethod, TestCategory("Unit")]
+    public void ShouldReturnAmountForBaseRentalOfOneDay_As200()
+    {
+        //assign
+        Price price = new Price();
+        //act
+        int numberOfDaysRented = 1;
+        int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
+
+        //assert
+        Assert.AreEqual(200, amount);
+    }
+    [TestMethod, TestCategory("Unit")]
+    public void ShouldReturnAmountForBaseRentalOfTwoDays_As200()
+    {
+        //assign
+        Price price = new Price();
+        //act
+        int numberOfDaysRented = 2;
+        int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
+
+        //assert
+        Assert.AreEqual(200, amount);
+    }
+    [TestMethod, TestCategory("Unit")]
+    public void ShouldReturnAmountForBaseRentalOfThreeDays_As300()
+    {
+        //assign
+        Price price = new Price();
+        //act
+        int numberOfDaysRented = 3;
+        int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
+
+        //assert
+        Assert.AreEqual(300, amount);
+    }
+    
+}
+
+public class Price
+{
+    public int RentalPriceForNumberOfDays(int numberOfDaysRented)
+    {
+        if (numberOfDaysRented > 2)
+        {
+            return (int)(200 * 1.5);
+        }
+        return 200;
+    }
+}
