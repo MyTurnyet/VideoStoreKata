44e3d8e4e33ae33ad3908625684c2a5279a6d21c
Paige Watson <paige.watson@outlook.com>
2022-04-21 15:11:23 -0700

    Add TextStatement

---
+++ TextStatement.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace VideoStore;
+
+public class TextStatement
+{
+    private readonly Customer _customer;
+
+    public TextStatement(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public string Output()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("Rental Record for "+_customer.Name);
+        stringBuilder.AppendLine("You owed 0.00");
+        stringBuilder.AppendLine("You earned 0 frequent renter points");
+        return stringBuilder.ToString();
+    }
+}
---
+++ TextStatementTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VideoStore;
+
+[TestClass]
+public class TextStatementTests
+{
+    [TestMethod, TestCategory("Unit")]
+    public void ShouldReturnStatementWithHeaderAndFooter()
+    {
+        //assign
+        Customer customer = new Customer() { Name = "Mike" };
+        TextStatement statement = new TextStatement(customer);
+        //act
+        string output = statement.Output();
+        //assert
+        Assert.AreEqual("Rental Record for Mike\r\n" +
+                        "You owed 0.00\r\n" +
+                        "You earned 0 frequent renter points\r\n", output);
+    }
+}
