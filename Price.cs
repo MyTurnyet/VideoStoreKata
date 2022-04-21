3ad236ff3ab8c7d0cbd9e9f7a9cfbbe71b841d7d
Paige Watson <paige.watson@outlook.com>
2022-04-21 12:22:16 -0700

    move price to separate file

---
+++ Price.cs
@@ -0,0 +1,14 @@
+﻿namespace VideoStore;
+
+public class Price
+{
+    public int BaseNumberOfRentalDays { private get; init; } = 2;
+    public int BaseRentalAmount { private get; init; } = 200;
+    public double OverDueMultiplier { private get; init; } = 1.5;
+    public int RentalPriceForNumberOfDays(int numberOfDaysRented)
+    {
+        if (numberOfDaysRented <= BaseNumberOfRentalDays) return BaseRentalAmount;
+        return (int)(BaseRentalAmount * OverDueMultiplier);
+
+    }
+}
--- PriceTests.cs
+++ PriceTests.cs
@@ -44,16 +44,3 @@ public class PriceTests
         Assert.AreEqual(300, amount);
     }
 }
-
-public class Price
-{
-    public int BaseNumberOfRentalDays { private get; init; } = 2;
-    public int BaseRentalAmount { private get; init; } = 200;
-    public double OverDueMultiplier { private get; init; } = 1.5;
-    public int RentalPriceForNumberOfDays(int numberOfDaysRented)
-    {
-        if (numberOfDaysRented <= BaseNumberOfRentalDays) return BaseRentalAmount;
-        return (int)(BaseRentalAmount * OverDueMultiplier);
-
-    }
-}
