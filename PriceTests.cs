df9ee719acf3709997b5427fba3a99f6d6ea3aa9
Paige Watson <paige.watson@outlook.com>
2022-04-21 12:21:40 -0700

    add properties for all variables in Price

--- PriceTests.cs
+++ PriceTests.cs
@@ -9,7 +9,7 @@ public class PriceTests
     public void ShouldReturnAmountForBaseRentalOfOneDay_As200()
     {
         //assign
-        Price price = new Price { BaseNumberOfRentalDays = 2 };
+        Price price = new Price { BaseNumberOfRentalDays = 2, BaseRentalAmount = 200, OverDueMultiplier = 1.5 };
         //act
         int numberOfDaysRented = 1;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -22,7 +22,7 @@ public class PriceTests
     public void ShouldReturnAmountForBaseRentalOfTwoDays_As200()
     {
         //assign
-        Price price = new Price { BaseNumberOfRentalDays = 2 };
+        Price price = new Price { BaseNumberOfRentalDays = 2, BaseRentalAmount = 200, OverDueMultiplier = 1.5 };
         //act
         int numberOfDaysRented = 2;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -35,7 +35,7 @@ public class PriceTests
     public void ShouldReturnAmountForBaseRentalOfThreeDays_As300()
     {
         //assign
-        Price price = new Price { BaseNumberOfRentalDays = 2 };
+        Price price = new Price { BaseNumberOfRentalDays = 2, BaseRentalAmount = 200, OverDueMultiplier = 1.5};
         //act
         int numberOfDaysRented = 3;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -48,15 +48,12 @@ public class PriceTests
 public class Price
 {
     public int BaseNumberOfRentalDays { private get; init; } = 2;
-
-
+    public int BaseRentalAmount { private get; init; } = 200;
+    public double OverDueMultiplier { private get; init; } = 1.5;
     public int RentalPriceForNumberOfDays(int numberOfDaysRented)
     {
-        if (numberOfDaysRented > BaseNumberOfRentalDays)
-        {
-            return (int)(200 * 1.5);
-        }
+        if (numberOfDaysRented <= BaseNumberOfRentalDays) return BaseRentalAmount;
+        return (int)(BaseRentalAmount * OverDueMultiplier);
 
-        return 200;
     }
 }
