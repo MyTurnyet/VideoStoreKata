b5371da825e16849185bff1b9d133c693b7d77a8
Paige Watson <paige.watson@outlook.com>
2022-04-21 12:17:02 -0700

    refactor to create field

--- PriceTests.cs
+++ PriceTests.cs
@@ -9,7 +9,7 @@ public class PriceTests
     public void ShouldReturnAmountForBaseRentalOfOneDay_As200()
     {
         //assign
-        Price price = new Price();
+        Price price = new Price { BaseNumberOfRentalDays = 2 };
         //act
         int numberOfDaysRented = 1;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -17,11 +17,12 @@ public class PriceTests
         //assert
         Assert.AreEqual(200, amount);
     }
+
     [TestMethod, TestCategory("Unit")]
     public void ShouldReturnAmountForBaseRentalOfTwoDays_As200()
     {
         //assign
-        Price price = new Price();
+        Price price = new Price { BaseNumberOfRentalDays = 2 };
         //act
         int numberOfDaysRented = 2;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -29,11 +30,12 @@ public class PriceTests
         //assert
         Assert.AreEqual(200, amount);
     }
+
     [TestMethod, TestCategory("Unit")]
     public void ShouldReturnAmountForBaseRentalOfThreeDays_As300()
     {
         //assign
-        Price price = new Price();
+        Price price = new Price { BaseNumberOfRentalDays = 2 };
         //act
         int numberOfDaysRented = 3;
         int amount = price.RentalPriceForNumberOfDays(numberOfDaysRented);
@@ -41,17 +43,20 @@ public class PriceTests
         //assert
         Assert.AreEqual(300, amount);
     }
-    
 }
 
 public class Price
 {
+    public int BaseNumberOfRentalDays { private get; init; } = 2;
+
+
     public int RentalPriceForNumberOfDays(int numberOfDaysRented)
     {
-        if (numberOfDaysRented > 2)
+        if (numberOfDaysRented > BaseNumberOfRentalDays)
         {
             return (int)(200 * 1.5);
         }
+
         return 200;
     }
 }
